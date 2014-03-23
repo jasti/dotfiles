320fa52fe6020fed9c005e9109927d493ccd4ca5
jasti <vamseej@gmail.com>
2014-03-23 12:55:14 -0400

    include tomcat in path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export MAVEN_HOME="/Users/jasti/software/apache-maven-3.2.1/bin"
-export PATH="./bin:$HOME/.rvm/scrips/rvm:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$MAVEN_HOME:$PATH"
+export TOMCAT_HOME="/Users/jasti/software/apache-tomcat-7.0.47/bin"
+export PATH="./bin:$HOME/.rvm/scrips/rvm:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$TOMCAT_HOME:$MAVEN_HOME:$PATH"
