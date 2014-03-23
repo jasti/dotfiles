5b2988bc1a10ead636806d6993f059bc79f4adaa
jasti <vamseej@gmail.com>
2014-03-23 12:47:06 -0400

    Include maven in path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,3 @@
-export PATH="./bin:$HOME/.rvm/scrips/rvm:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export MAVEN_HOME="/Users/jasti/software/apache-maven-3.2.1/bin"
+export PATH="./bin:$HOME/.rvm/scrips/rvm:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$MAVEN_HOME:$PATH"
